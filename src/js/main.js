073bbdb5fda8d1edad8a3cf4af498717ca565ede
eguvenc <eguvenc@gmail.com>
2024-11-11 20:29:44 +0300

    added font.css

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,9 @@
 
 
 // Import all of Bootstrap's JS
-import * as bootstrap from 'bootstrap'
+// import * as bootstrap from 'bootstrap'
+
+import Alert from 'bootstrap/js/dist/alert';
+import Buttons from 'bootstrap/js/dist/button';
+import Dropdown from 'bootstrap/js/dist/dropdown';
+import Tabs from 'bootstrap/js/dist/tab';
